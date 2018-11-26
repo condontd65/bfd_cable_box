a9c99f2da36122c04a86ef2e84a95636284cc1e7
Timothy Condon <151268@cityhall.boston.cob>
2018-11-26 15:19:17 -0500

    Adding additional test scripts

--- cable_test.py
+++ cable_test.py
@@ -1,8 +1,8 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
+# Name:        cable_test
 # Purpose:
 #
-# Author:      151268
+# Author:      151268 - Timothy Condon
 #
 # Created:     26/11/2018
 # Copyright:   (c) 151268 2018
@@ -10,9 +10,32 @@
 #-------------------------------------------------------------------------------
 
 import arcpy
+import os
+import xlrd
+
 arcpy.env.overwriteOutput = True
 
 arcpy.env.workspace = "c:/Users/151268/Desktop/TC_FAO/firebox/box_cable_test.gdb"
 
 #in_feature_class = 'cable_features'
-#target_workspace = 'c:/Users/151268/Desktop/TC_FAO/firebox/box_cable_work.gdb'
+target_workspace = 'c:/Users/151268/Desktop/TC_FAO/firebox/box_cable_work.gdb'
+
+def importallsheets(in_excel, out_gdb):
+    workbook = xlrd.open_workbook(in_excel)
+    sheets = [sheet.name for sheet in workbook.sheets()]
+    print('{} sheets found: {}'.format(len(sheets), ','.join(sheets)))
+    for sheet in sheets:
+        # The out_table s based on the input excel filename
+        #an underscore seperator followed by the sheet name
+        out_table = os.path.join(
+            out_gdb,
+            arcpy.ValidateTableName(
+                "{0}_{1}".format(os.path.basename(in_excel), sheet),
+                out_gdb))
+        print('Converting {} to {}'.format(sheet, out_table))
+
+        # Perform the conversion
+        arcpy.ExcelToTable_conversion(in_excel, out_table, sheet)
+
+if __name__ == '__main__':
+    importallsheets('in_file','gdb')
---
+++ test_error_issue.py
@@ -0,0 +1,41 @@
+
+import arcinfo
+import arcpy, sys, traceback
+import os
+import xlrd
+from arcpy import env
+
+env.overwriteOutput = True
+
+env.workspace = "c:/test/test.gdb"
+
+#set variables
+input_table = "c:/Users/151268/Documents/BFD/tables/cable_test_only2.csv"
+x_coord = "long"
+y_coord = "lat"
+out_layer = "c2_feature_test"
+#save_layer = "c2_feature_test.lyr"
+save_points = "test_code_points"
+out_lines = "test_code_lines"
+sp_ref = arcpy.SpatialReference(2249)
+
+
+#arcpy.ExcelToTable_conversion(input_table,
+#    "c:/Users/151268/Documents/BFD/tables/box_cable_test.gdb", "Sheet1")
+#print("Table conversion complete")
+
+
+arcpy.MakeXYEventLayer_management(input_table, x_coord, y_coord, out_layer, sp_ref)
+print("Point event generated")
+
+arcpy.CopyFeatures_management(out_layer, save_points)
+
+
+#arcpy.SaveToLayerFile_management(out_layer, save_layer)
+
+#arcpy.FeatureToPoint_management(out_layer, save_points)
+#print("Points generated")
+
+arcpy.PointsToLine_management(save_points, out_lines, Sort_Field = "circuit_ord")
+print("Line generated")
+
